ca0828f7211eaf491b39c164e2b25b5ab644d022
Megidy <byrka.sergiy@gmail.com>
2024-10-25 18:41:21 +0300

    Added config initialization.

---
+++ config/env.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"os"
+)
+
+type Config struct {
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBProtocol string
+	DB         string
+	DBPort     string
+}
+
+func InitConfig() *Config {
+	return &Config{
+		DBUser:     GetEnv("DB_USER"),
+		DBPassword: GetEnv("DB_PASSWORD"),
+		DBName:     GetEnv("DB_NAME"),
+		DBProtocol: GetEnv("DB_PROTOCOL"),
+		DB:         GetEnv("DB"),
+		DBPort:     GetEnv("DB_PORT"),
+	}
+}
+
+func GetEnv(key string) string {
+	value := os.Getenv(key)
+	return value
+
+}
