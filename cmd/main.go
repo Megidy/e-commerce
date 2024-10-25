ca4c905c8b1a3c62ee35bae0d5698e2edca07adb
Megidy <byrka.sergiy@gmail.com>
2024-10-25 18:43:00 +0300

    Initializated connections between api and db.

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Megidy/e-commerce/cmd/api"
+	"github.com/Megidy/e-commerce/db"
+)
+
 func main() {
+	db, err := db.NewMySQlStorage()
+	if err != nil {
+		log.Fatal(err)
+	}
+	server := api.NewApiServer(":8080", db)
+	err = server.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
