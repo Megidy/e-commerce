e2ce6f6346564e12a2b6100ebd5a2ef7ae33e5a0
Megidy <byrka.sergiy@gmail.com>
2024-10-25 18:42:16 +0300

    Added NewApiServe func and method Run.

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,8 +1,28 @@
 package api
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 type APIServer struct {
 	addr string
 	db   *sql.DB
+}
+
+func NewApiServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := gin.Default()
+
+	log.Println("Started Server on port :8080")
+	return router.Run(s.addr)
+
 }
