c546dedaf0a7a49f179b2903d1d7ad8bc5a1b039
Megidy <byrka.sergiy@gmail.com>
2024-10-25 18:41:42 +0300

    Added DB connection.

--- db/db.go
+++ db/db.go
@@ -2,14 +2,37 @@ package db
 
 import (
 	"database/sql"
+	"log"
 
+	"github.com/Megidy/e-commerce/config"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func NewPostgreSQLStorage(connectionSrt string) (*sql.DB, error) {
-	db, err := sql.Open("mysql", "root:password@/productdb")
+func NewMySQlStorage() (*sql.DB, error) {
+	dsn := GetDSN()
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+	err = InitStorage(db)
+	if err != nil {
+		return nil, err
+	}
+
 	return db, nil
+}
+
+func GetDSN() string {
+	config := *config.InitConfig()
+	log.Println(config)
+	return config.DBUser + ":" + config.DBPassword + "@" + config.DBProtocol + "(" + config.DB + ":" + config.DBPort + ")" + "/" + config.DBName
+}
+
+func InitStorage(db *sql.DB) error {
+	err := db.Ping()
+	if err != nil {
+		return err
+	}
+	log.Println("Success: DB is UP")
+	return nil
 }
