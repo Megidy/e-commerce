6273e69a36e1ff60c8977bd3d601b43d487fae4d
Megidy <byrka.sergiy@gmail.com>
2024-10-26 22:29:34 +0300

    Added roles for user,loginPayload,loadResponse and new interfaces for user store.

--- types/types.go
+++ types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "github.com/gin-gonic/gin"
+
 type User struct {
 	ID       string
 	Name     string
@@ -7,6 +9,7 @@ type User struct {
 	Email    string
 	Password string
 	Created  string
+	Role     string
 }
 
 type CreateUserPayload struct {
@@ -15,7 +18,19 @@ type CreateUserPayload struct {
 	Email    string
 	Password string
 }
+
+type LogInPayload struct {
+	Email    string
+	Password string
+}
+
 type UserStore interface {
 	CreateUser(user *User) error
 	AlreadyExists(user *User) (bool, error)
+	GetUserByEmail(email string) (User, error)
+	GetUserById(id string) (User, error)
+}
+
+type LoadResponse interface {
+	LoadResponse(c *gin.Context, statusCode int, file string, response map[string]any)
 }
