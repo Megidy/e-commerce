a8721876f9499fec8d0b6c690394b64cf481fe3a
Megidy <byrka.sergiy@gmail.com>
2024-10-26 13:30:51 +0300

    Added new user handler and loaded glob files.

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/Megidy/e-commerce/services/user"
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,8 +21,12 @@ func NewApiServer(addr string, db *sql.DB) *APIServer {
 }
 
 func (s *APIServer) Run() error {
-	router := gin.Default()
 
+	router := gin.Default()
+	router.LoadHTMLGlob("./frontend/templates/*.html")
+	userStore := user.NewStore(s.db)
+	userHandler := user.NewHandler(userStore)
+	userHandler.RegisterRoutes(router)
 	log.Println("Started Server on port :8080")
 	return router.Run(s.addr)
 
