d3844df5164465fd810a749e664b08c4fcc6667a
Megidy <byrka.sergiy@gmail.com>
2024-10-26 20:22:55 +0300

    Added new dependency : response loader which should load html template

--- services/user/routes.go
+++ services/user/routes.go
@@ -6,40 +6,53 @@ import (
 
 	"github.com/Megidy/e-commerce/services/auth"
 	"github.com/Megidy/e-commerce/types"
-	"github.com/Megidy/e-commerce/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
 type Handler struct {
+	response  types.LoadResponse
 	userStore types.UserStore
 }
 
-func NewHandler(userStore types.UserStore) *Handler {
+func NewHandler(response types.LoadResponse, userStore types.UserStore) *Handler {
 	return &Handler{
+		response:  response,
 		userStore: userStore,
 	}
 }
 func (h *Handler) RegisterRoutes(router gin.IRouter) {
-	router.Any("/signup", h.SignUp)
+	router.GET("/signup", h.SignUp)
+	router.POST("/signup", h.SignUp)
 
 }
 func (h *Handler) SignUp(c *gin.Context) {
 	if c.Request.Method == http.MethodGet {
-		c.HTML(http.StatusOK, "signup.html", nil)
-	} else if c.Request.Method == http.MethodPost {
+		h.response.LoadResponse(c, http.StatusOK, "signup.html", nil)
+		return
+	}
+
+	if c.Request.Method == http.MethodPost {
 		var payload types.CreateUserPayload
 		var user types.User
 		err := c.ShouldBindJSON(&payload)
 		if err != nil {
-			utils.HandleError(c, err, "failed to read", http.StatusBadRequest)
+			h.response.LoadResponse(c, http.StatusBadRequest, "signup.html", gin.H{
+				"Error": err.Error(),
+			})
 			return
+			// utils.HandleError(c, err, "failed to read", http.StatusBadRequest)
+			// return
 		}
 		log.Println(payload)
 		hashedPassword, err := auth.HashPassword(payload.Password)
 		if err != nil {
-			utils.HandleError(c, err, "failed to hash password", http.StatusBadRequest)
+			h.response.LoadResponse(c, http.StatusBadRequest, "signup.html", gin.H{
+				"Error": err.Error(),
+			})
 			return
+			// utils.HandleError(c, err, "failed to hash password", http.StatusBadRequest)
+			// return
 		}
 		user = types.User{
 			ID:       uuid.NewString(),
@@ -50,22 +63,29 @@ func (h *Handler) SignUp(c *gin.Context) {
 		}
 		ok, err := h.userStore.AlreadyExists(&user)
 		if err != nil {
-			utils.HandleError(c, err, "", http.StatusBadRequest)
+			h.response.LoadResponse(c, http.StatusBadRequest, "signup.html", gin.H{
+				"Error": err.Error(),
+			})
 			return
-
+			// utils.HandleError(c, err, "", http.StatusBadRequest)
+			// return
 		}
+
 		if ok {
-
-			utils.HandleError(c, nil, "email is already been used", http.StatusBadRequest)
+			h.response.LoadResponse(c, http.StatusBadRequest, "signup.html", gin.H{
+				"Response": "Email is already in use",
+			})
 			return
-
 		}
+
 		err = h.userStore.CreateUser(&user)
 		if err != nil {
-			utils.HandleError(c, err, "", http.StatusInternalServerError)
+			h.response.LoadResponse(c, http.StatusBadRequest, "signup.html", gin.H{
+				"Error": err.Error(),
+			})
 			return
 		}
 
+		c.Redirect(http.StatusSeeOther, "/products")
 	}
-	c.Redirect(http.StatusFound, "/products")
 }
