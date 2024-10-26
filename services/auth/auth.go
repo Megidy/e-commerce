7cbb64c0dd069d13172c06e2a46091bc7ca842bc
Megidy <byrka.sergiy@gmail.com>
2024-10-26 13:31:01 +0300

    Added password hashing.

--- services/auth/auth.go
+++ services/auth/auth.go
@@ -1,1 +1,7 @@
 package auth
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
