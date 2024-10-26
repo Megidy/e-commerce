3e9aa51e6b98c162a982f18710199e1b83bba207
Megidy <byrka.sergiy@gmail.com>
2024-10-26 13:29:59 +0300

    Added error handling and response sending.

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,27 @@
 package utils
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HandleError(c *gin.Context, err error, message string, statusCode int) {
+	if err != nil {
+		c.JSON(statusCode, gin.H{
+			"error":   err.Error(),
+			"details": message,
+		})
+		log.Println(err)
+		log.Println(err.Error())
+	} else {
+		c.JSON(statusCode, gin.H{
+			"details": message,
+		})
+	}
+}
+func SendResponse(c *gin.Context, message string, statusCode int) {
+	c.JSON(statusCode, gin.H{
+		"message": message,
+	})
+}
