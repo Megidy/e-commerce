3459def728601852aecdfe8a7b5975ee7f7394cb
Megidy <byrka.sergiy@gmail.com>
2024-10-28 18:42:54 +0200

    Changed html rendering via package templates.

--- services/user/routes.go
+++ services/user/routes.go
@@ -12,113 +12,110 @@ import (
 )
 
 type Handler struct {
-	response  types.LoadResponse
+	templates types.Templates
 	userStore types.UserStore
 }
 
-func NewHandler(response types.LoadResponse, userStore types.UserStore) *Handler {
+func NewHandler(templates types.Templates, userStore types.UserStore) *Handler {
 	return &Handler{
-		response:  response,
+		templates: templates,
 		userStore: userStore,
 	}
 }
 func (h *Handler) RegisterRoutes(router gin.IRouter) {
-	router.Any("/signup", h.SignUp)
-	router.Any("/login", h.LogIn)
+	router.GET("/signup", h.LoadSignUpHTML)
+	router.POST("/signup/create", h.SignUp)
+	router.GET("/login", h.LoadLogInHTML)
+	router.POST("/login/enter", h.LogIn)
+
 }
+
 func (h *Handler) SignUp(c *gin.Context) {
-	if c.Request.Method == http.MethodGet {
-		h.response.LoadResponse(c, http.StatusOK, "signup.html", nil)
+	var payload types.CreateUserPayload
+	var user types.User
+	payload.Name = h.templates.GetDataFromForm(c, "name")
+	payload.LastName = h.templates.GetDataFromForm(c, "lastname")
+	payload.Email = h.templates.GetDataFromForm(c, "email")
+	payload.Password = h.templates.GetDataFromForm(c, "password")
+	log.Println(payload)
+	ok, err := h.userStore.AlreadyExists(&types.User{Email: payload.Email})
+	if err != nil {
+		h.templates.ExecuteSpecificTemplate(c, "message-after-submit", "frontend/templates/signup.html", map[string]any{
+			"Message": err.Error(),
+		})
+		return
+	}
+	if ok {
+		h.templates.ExecuteSpecificTemplate(c, "message-after-submit", "frontend/templates/signup.html", map[string]any{
+			"Message": "Email is already taken",
+		})
+		return
+	}
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		h.templates.ExecuteSpecificTemplate(c, "message-after-submit", "frontend/templates/signup.html", map[string]any{
+			"Message": err.Error(),
+		})
+		return
+	}
+	user = types.User{
+		ID:       uuid.NewString(),
+		Name:     payload.Name,
+		LastName: payload.LastName,
+		Email:    payload.Email,
+		Password: string(hashedPassword),
+		Role:     "User",
+	}
+	err = h.userStore.CreateUser(&user)
+	if err != nil {
+		h.templates.ExecuteSpecificTemplate(c, "message-after-submit", "frontend/templates/signup.html", map[string]any{
+			"Message": err.Error(),
+		})
 		return
 	}
 
-	if c.Request.Method == http.MethodPost {
-		var payload types.CreateUserPayload
-		var user types.User
-		err := c.ShouldBindJSON(&payload)
-		if err != nil {
-			h.response.LoadResponse(c, http.StatusBadRequest, "signup.html", gin.H{
-				"Error": err.Error(),
-			})
-			return
-			// utils.HandleError(c, err, "failed to read", http.StatusBadRequest)
-			// return
-		}
-		log.Println(payload)
-		hashedPassword, err := auth.HashPassword(payload.Password)
-		if err != nil {
-			h.response.LoadResponse(c, http.StatusBadRequest, "signup.html", gin.H{
-				"Error": err.Error(),
-			})
-			return
-			// utils.HandleError(c, err, "failed to hash password", http.StatusBadRequest)
-			// return
-		}
-		user = types.User{
-			ID:       uuid.NewString(),
-			Name:     payload.Name,
-			LastName: payload.LastName,
-			Email:    payload.Email,
-			Password: string(hashedPassword),
-		}
-		ok, err := h.userStore.AlreadyExists(&user)
-		if err != nil {
-			h.response.LoadResponse(c, http.StatusBadRequest, "signup.html", gin.H{
-				"Error": err.Error(),
-			})
-			return
-			// utils.HandleError(c, err, "", http.StatusBadRequest)
-			// return
-		}
+	h.templates.ExecuteSpecificTemplate(c, "message-after-submit", "frontend/templates/signup.html", map[string]any{
+		"Message": "User Signed In",
+	})
+}
 
-		if ok {
-			h.response.LoadResponse(c, http.StatusBadRequest, "signup.html", gin.H{
-				"Message": "Email is already in use",
-			})
-			return
-		}
+func (h *Handler) LoadSignUpHTML(c *gin.Context) {
+	h.templates.ExecuteTemplate(c, "frontend/templates/signup.html", nil)
+}
+func (h *Handler) LoadLogInHTML(c *gin.Context) {
 
-		err = h.userStore.CreateUser(&user)
-		if err != nil {
-			h.response.LoadResponse(c, http.StatusBadRequest, "signup.html", gin.H{
-				"Error": err.Error(),
-			})
-			return
-		}
-
-		c.Redirect(http.StatusSeeOther, "/login")
-	}
 }
 
 func (h *Handler) LogIn(c *gin.Context) {
 	if c.Request.Method == http.MethodGet {
-		h.response.LoadResponse(c, http.StatusOK, "login.html", nil)
+		h.templates.LoadResponse(c, http.StatusOK, "login.html", nil)
 	} else if c.Request.Method == http.MethodPost {
 		var logInPayload types.LogInPayload
 		c.ShouldBindJSON(&logInPayload)
 		ok, err := h.userStore.AlreadyExists(&types.User{Email: logInPayload.Email})
 		if err != nil {
-			h.response.LoadResponse(c, http.StatusBadRequest, "login.html", gin.H{
+			h.templates.LoadResponse(c, http.StatusBadRequest, "login.html", gin.H{
 				"Error": err.Error(),
 			})
 			return
 		}
 		if !ok {
-			h.response.LoadResponse(c, http.StatusBadRequest, "login.html", gin.H{
+			h.templates.LoadResponse(c, http.StatusBadRequest, "login.html", gin.H{
 				"Message": "no user with this email found ",
 			})
+
 			return
 		} else if ok {
 			user, err := h.userStore.GetUserByEmail(logInPayload.Email)
 			if err != nil {
-				h.response.LoadResponse(c, http.StatusBadRequest, "login.html", gin.H{
+				h.templates.LoadResponse(c, http.StatusBadRequest, "login.html", gin.H{
 					"Error": err.Error(),
 				})
 				return
 			}
 			ok := auth.ComparePassword(user.Password, logInPayload.Password)
 			if !ok {
-				h.response.LoadResponse(c, http.StatusBadRequest, "login.html", gin.H{
+				h.templates.LoadResponse(c, http.StatusBadRequest, "login.html", gin.H{
 					"Error": "Invlid data sent",
 				})
 				return
@@ -127,7 +124,7 @@ func (h *Handler) LogIn(c *gin.Context) {
 			config := config.InitConfig()
 			secret, err := auth.CreateJWT([]byte(config.Secret), user.ID)
 			if err != nil {
-				h.response.LoadResponse(c, http.StatusBadRequest, "login.html", gin.H{
+				h.templates.LoadResponse(c, http.StatusBadRequest, "login.html", gin.H{
 					"Error": err.Error(),
 				})
 				return
