c9b893556818dfc0ce5041cbab1885b59dd88ac1
Megidy <byrka.sergiy@gmail.com>
2024-10-28 18:43:10 +0200

    Added new functions for render templates.

--- frontend/response/response.go
+++ frontend/response/response.go
@@ -2,19 +2,35 @@ package response
 
 import (
 	"log"
+	"text/template"
 
 	"github.com/gin-gonic/gin"
 )
 
-type ResponseHandler struct {
+type TemplateHandler struct {
 }
 
-func NewResponseHandler() *ResponseHandler {
-	return &ResponseHandler{}
+func NewTemplateHandler() *TemplateHandler {
+	return &TemplateHandler{}
 }
 
-func (h *ResponseHandler) LoadResponse(c *gin.Context, statusCode int, file string, response map[string]any) {
+func (h *TemplateHandler) LoadResponse(c *gin.Context, statusCode int, file string, response map[string]any) {
 	log.Println(response)
 	c.HTML(statusCode, file, response)
 
+}
+
+func (h *TemplateHandler) ExecuteTemplate(c *gin.Context, filePath string, data map[string]any) error {
+	tmpl := template.Must(template.ParseFiles(filePath))
+
+	return tmpl.Execute(c.Writer, data)
+}
+
+func (h *TemplateHandler) ExecuteSpecificTemplate(c *gin.Context, name, filePath string, data map[string]any) error {
+	tmpl := template.Must(template.ParseFiles(filePath))
+	return tmpl.ExecuteTemplate(c.Writer, name, data)
+}
+
+func (h *TemplateHandler) GetDataFromForm(c *gin.Context, key string) string {
+	return c.Request.PostFormValue(key)
 }
