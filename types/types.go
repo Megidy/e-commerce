681b1822953cda08d0aa7c1c25af791163abee7a
Megidy <byrka.sergiy@gmail.com>
2024-10-28 18:43:23 +0200

    Added new Interfaces for templates.

--- types/types.go
+++ types/types.go
@@ -31,6 +31,9 @@ type UserStore interface {
 	GetUserById(id string) (User, error)
 }
 
-type LoadResponse interface {
+type Templates interface {
 	LoadResponse(c *gin.Context, statusCode int, file string, response map[string]any)
+	ExecuteTemplate(c *gin.Context, filePath string, data map[string]any) error
+	ExecuteSpecificTemplate(c *gin.Context, name, filePath string, data map[string]any) error
+	GetDataFromForm(c *gin.Context, key string) string
 }
