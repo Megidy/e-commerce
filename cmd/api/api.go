6b27dafc0b273ef5f81b92b154f88f9d3ecb1e1a
Megidy <byrka.sergiy@gmail.com>
2024-10-31 23:52:23 +0200

    Added new Store - products and injected to products service.

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/Megidy/e-commerce/frontend/response"
+	"github.com/Megidy/e-commerce/services/product"
 	"github.com/Megidy/e-commerce/services/user"
 	"github.com/gin-gonic/gin"
 )
@@ -25,12 +26,19 @@ func (s *APIServer) Run() error {
 
 	router := gin.Default()
 	// router.LoadHTMLGlob("./frontend/templates/*.html")
+
+	userStore := user.NewStore(s.db)
+	productStore := product.NewStore(s.db)
+
 	NewResponseHandler := response.NewTemplateHandler()
 	router.RedirectFixedPath = true
 	router.RedirectTrailingSlash = true
-	userStore := user.NewStore(s.db)
+
 	userHandler := user.NewHandler(NewResponseHandler, userStore)
 	userHandler.RegisterRoutes(router)
+	productHandler := product.NewHandler(userStore, productStore)
+	productHandler.RegisterRoutes(router)
+
 	log.Println("Started Server on port :8080")
 	return router.Run(s.addr)
 
