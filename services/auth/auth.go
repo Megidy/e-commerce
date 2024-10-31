cf3eb85ba3fd6330369cd2b3589b3a752939aace
Megidy <byrka.sergiy@gmail.com>
2024-10-31 15:03:57 +0200

    Deleted files.

--- services/auth/auth.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-import "golang.org/x/crypto/bcrypt"
-
-func HashPassword(password string) ([]byte, error) {
-	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-}
