9cb7ba002634a577f686c048b9a356ea5370d9ba
Megidy <byrka.sergiy@gmail.com>
2024-10-31 23:53:41 +0200

    Added new service products.

--- services/product/routes.go
+++ services/product/routes.go
@@ -1,4 +1,34 @@
 package product
 
+import (
+	"log"
+
+	templates "github.com/Megidy/e-commerce/frontend/templates/product"
+	"github.com/Megidy/e-commerce/types"
+	"github.com/gin-gonic/gin"
+)
+
 type Handler struct {
+	userStore    types.UserStore
+	productStore types.ProductStore
+}
+
+func NewHandler(userStore types.UserStore, productStore types.ProductStore) *Handler {
+	return &Handler{
+		userStore:    userStore,
+		productStore: productStore,
+	}
+}
+
+func (h *Handler) RegisterRoutes(router gin.IRouter) {
+	router.GET("/products/accessories", h.GetAllAccessories)
+}
+
+func (h *Handler) GetAllAccessories(c *gin.Context) {
+	accessories, err := h.productStore.GetAllAccessories()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	templates.LoadAccessories(accessories).Render(c.Request.Context(), c.Writer)
 }
