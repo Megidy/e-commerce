3cdea5933526ea918c64c6562160cd00b7f78ee4
Megidy <byrka.sergiy@gmail.com>
2024-11-11 21:51:01 +0200

    Added db query for creating order.

--- services/order/store.go
+++ services/order/store.go
@@ -1,1 +1,32 @@
 package order
+
+import (
+	"database/sql"
+
+	"github.com/Megidy/e-commerce/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) CreateOrder(order types.Order, cart []types.Cart) error {
+
+	_, err := s.db.Exec("insert into orders(order_id,user_id,status) values(?,?,?)", order.Order_id, order.User_id, order.Status)
+	if err != nil {
+		return err
+	}
+
+	for _, c := range cart {
+		_, err := s.db.Exec("insert into orders_products(order_id,product_id,quantity) values(?,?,?)", order.Order_id, c.Product_id, c.Quantity)
+		if err != nil {
+			return err
+
+		}
+	}
+	return nil
+}
