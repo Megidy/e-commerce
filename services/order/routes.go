2bfa77b2c028e2f67e161dacc45e846bb9f3f682
Megidy <byrka.sergiy@gmail.com>
2024-11-16 19:55:10 +0200

    Added GetOrders route, Changed CreateOrder , added auth as parametr for func.

--- services/order/routes.go
+++ services/order/routes.go
@@ -3,6 +3,7 @@ package order
 import (
 	"log"
 
+	"github.com/Megidy/e-commerce/frontend/templates/order"
 	"github.com/Megidy/e-commerce/services/auth"
 	"github.com/Megidy/e-commerce/types"
 	"github.com/gin-gonic/gin"
@@ -17,12 +18,12 @@ type Handler struct {
 }
 
 func NewHandler(userStore types.UserStore, orderStore types.OrderStore, productStore types.ProductStore, cartStore types.CartStore) *Handler {
-	return &Handler{userStore: userStore, orderStore: orderStore, cartStore: cartStore}
+	return &Handler{userStore: userStore, orderStore: orderStore, productStore: productStore, cartStore: cartStore}
 }
 
-func (h *Handler) RegisterRoutes(router gin.IRouter) {
-	router.POST("/orders/createorder", auth.NewHandler(h.userStore).WithJWT, h.CreateOrder)
-	router.GET("/orders")
+func (h *Handler) RegisterRoutes(router gin.IRouter, authHandler *auth.Handler) {
+	router.POST("/orders/createorder", authHandler.WithJWT, h.CreateOrder)
+	router.GET("/orders", authHandler.WithJWT, h.GetOrders)
 	router.GET("/orders/:orderID")
 	router.GET("orders/:orderID/:productID")
 	router.DELETE("/orders/:orderID/cancel")
@@ -30,6 +31,7 @@ func (h *Handler) RegisterRoutes(router gin.IRouter) {
 }
 
 func (h *Handler) CreateOrder(c *gin.Context) {
+	RedirectURL := "/cart"
 	var newOrder types.Order
 	u, ok := c.Get("user")
 	if !ok {
@@ -42,14 +44,27 @@ func (h *Handler) CreateOrder(c *gin.Context) {
 		log.Println(err)
 		return
 	}
-	_, _, totalPrice, err := h.productStore.GetAllProductsForCart(cart)
+	if cart == nil {
+		c.Writer.Header().Add("HX-Redirect", RedirectURL)
+		return
+	}
+	log.Println("cart : ", cart)
+	log.Println("user : ", user)
+	acc, bic, totalPrice, err := h.productStore.GetAllProductsForCart(cart)
+	log.Println("acc: ", acc, "bic: ", bic)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	for _, c := range cart {
-		err = h.cartStore.DeleteFromCart(c.UserId, c.Product_id)
+	for _, ca := range cart {
+		if ca.Quantity == 0 {
+
+			c.Writer.Header().Add("HX-Redirect", RedirectURL)
+			return
+		}
+		err = h.cartStore.DeleteFromCart(ca.UserId, ca.Product_id)
 		if err != nil {
+			log.Println(err)
 			return
 		}
 	}
@@ -64,7 +79,21 @@ func (h *Handler) CreateOrder(c *gin.Context) {
 		log.Println(err)
 		return
 	}
-	str := "/cart"
-	c.Writer.Header().Add("HX-Redirect", str)
+	c.Writer.Header().Add("HX-Redirect", RedirectURL)
 
+}
+
+func (h *Handler) GetOrders(c *gin.Context) {
+	u, ok := c.Get("user")
+	if !ok {
+		log.Println("user was not found ")
+		return
+	}
+	user := u.(types.User)
+	orders, err := h.orderStore.GetOrders(user.ID)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	order.LoadOrders(orders).Render(c.Request.Context(), c.Writer)
 }
