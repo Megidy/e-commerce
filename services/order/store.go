78fe83f6f8f98557c106d58510cffc05553e5ee3
Megidy <byrka.sergiy@gmail.com>
2024-11-17 20:14:48 +0200

    Separated function , added new queries.

--- services/order/store.go
+++ services/order/store.go
@@ -2,6 +2,7 @@ package order
 
 import (
 	"database/sql"
+	"log"
 
 	"github.com/Megidy/e-commerce/types"
 )
@@ -14,20 +15,24 @@ func NewStore(db *sql.DB) *Store {
 	return &Store{db: db}
 }
 
-func (s *Store) CreateOrder(order types.Order, cart []types.Cart) error {
+func (s *Store) CreateOrder(order types.Order) error {
 
 	_, err := s.db.Exec("insert into orders(order_id,user_id,status,total_price) values(?,?,?,?)", order.Order_id, order.User_id, order.Status, order.TotalPrice)
 	if err != nil {
 		return err
 	}
 
-	for _, c := range cart {
-		_, err := s.db.Exec("insert into orders_products(order_id,product_id,quantity) values(?,?,?)", order.Order_id, c.Product_id, c.Quantity)
-		if err != nil {
-			return err
+	return nil
+}
 
-		}
+func (s *Store) CreateOrdersProducts(order types.Order, cart types.Cart, totalPrice float32) error {
+	_, err := s.db.Exec("insert into orders_products(user_id,order_id,product_id,quantity,total_price) values(?,?,?,?,?)", cart.UserId, order.Order_id, cart.Product_id, cart.Quantity, totalPrice)
+
+	if err != nil {
+		return err
 	}
+	log.Printf("Inserting order_id: %s, product_id: %s, quantity: %d, total_price: %.2f\n", order.Order_id, cart.Product_id, cart.Quantity, totalPrice)
+
 	return nil
 }
 
@@ -48,4 +53,35 @@ func (s *Store) GetOrders(userID string) ([]types.Order, error) {
 	}
 	return orders, nil
 
+}
+func (s *Store) GetOrderById(userID, orderId string) (types.Order, error) {
+	row, err := s.db.Query("select * from orders where order_id=? and user_id=?", orderId, userID)
+	if err != nil {
+		return types.Order{}, nil
+	}
+	var order types.Order
+
+	for row.Next() {
+		err = row.Scan(&order.Order_id, &order.User_id, &order.Status, &order.Created, &order.TotalPrice)
+		if err != nil {
+			return types.Order{}, nil
+		}
+	}
+	return order, nil
+}
+func (s *Store) GetOrdersProducts(userID, orderID string) ([]types.OrderProduct, error) {
+	var products []types.OrderProduct
+	rows, err := s.db.Query("select * from orders_products where user_id=? and order_id=?", userID, orderID)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var product types.OrderProduct
+		err = rows.Scan(&product.User_id, &product.Order_id, &product.Product_id, &product.Quantity, &product.TotalPrice)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, product)
+	}
+	return products, nil
 }
